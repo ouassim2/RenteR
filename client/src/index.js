1fd7ecb5e662e911c1357323d88009344ee38bb0
Ouassim <112521258+ouassim2@users.noreply.github.com>
2023-03-26 12:48:44 -0400

    commented import error

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from "@auth0/auth0-react"
 import ToolProvider from './components/ToolContext';
 
