356495d23b54e2793901289acf4b1f576718cec4
Ouassim <112521258+ouassim2@users.noreply.github.com>
2023-03-31 18:49:04 -0400

    Created base64 img convert component

---
+++ client/src/components/ConvertToBase64.js
@@ -0,0 +1,26 @@
+
+const ConvertToBase64 = ({alt, onDone}) => {
+
+ const handleChange = (e) => {
+      e.preventDefault()
+      
+      let reader = new FileReader()
+      reader.readAsDataURL(e.target.files[0])
+      
+      reader.onload = () => {
+        let base64 = reader.result
+        onDone(base64)
+      };
+
+    }
+    
+    return ( 
+
+    <>
+      <input type='file' accept="image/*" alt={alt} onChange={handleChange}></input>
+    </> 
+    
+    );
+}
+ 
+export default ConvertToBase64;
